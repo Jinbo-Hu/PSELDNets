9d21eaa6986cab290a39683b7a962e4772cfea1b
Jinbo Hu <53830482+Jinbo-Hu@users.noreply.github.com>
2024-12-20 16:02:16 +0800

    Update prepare_starss23.bash

--- scripts/prepare_starss23.bash
+++ scripts/prepare_starss23.bash
@@ -2,7 +2,7 @@
 TGT_DIR='datasets/STARSS23'
 
 pip3 install zenodo_get
-zenodo_get 10.5281/zenodo.7880637 -d $TGT_DIR
+zenodo_get 10.5281/zenodo.7880637 -o $TGT_DIR
 unzip $TGT_DIR/foa_dev.zip -d $TGT_DIR
 unzip $TGT_DIR/mic_dev.zip -d $TGT_DIR
 unzip $TGT_DIR/metadata_dev.zip -d $TGT_DIR
